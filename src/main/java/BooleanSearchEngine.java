7671d187f747b46787a76004094bb628bf1df68c
Mark Krysoboy <heter1209@gmail.com>
2022-02-21 17:28:26 +0300

    Writed decision

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,53 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> wordInfMap;
+
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        wordInfMap = new HashMap<>();
+        if (pdfsDir.isDirectory()) {
+            for (File file : pdfsDir.listFiles()) {
+                if (file.isFile() && file.getName().endsWith(".pdf")) {
+                    try (var doc = new PdfDocument(new PdfReader(file))) {
+                        for (int i = 1; i <= doc.getPageNumber(doc.getLastPage()); i++) {
+                            var text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                            var words = text.split("\\P{IsAlphabetic}+");
+                            Set<String> wordSet = Arrays.stream(words)
+                                    .map(String::toLowerCase)
+                                    .collect(Collectors.toSet());
+                            List<PageEntry> pageEntryList;
+                            for (String word : wordSet) {
+                                int count = Collections.frequency(Arrays.stream(words).collect(Collectors.toList()), word);
+                                PageEntry pageEntry = new PageEntry(file.getName(), i, count);
+                                if (!wordInfMap.containsKey(word)) {
+                                    pageEntryList = new ArrayList<>();
+                                    pageEntryList.add(pageEntry);
+                                    wordInfMap.put(word, pageEntryList);
+                                } else {
+                                    wordInfMap.get(word).add(pageEntry);
+                                    Collections.sort(wordInfMap.get(word), Collections.reverseOrder());
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
+
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return wordInfMap.get(word.toLowerCase());
     }
 }
